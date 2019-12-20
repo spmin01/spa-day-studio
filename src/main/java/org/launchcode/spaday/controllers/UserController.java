d9585e4a96a8dd094fbf470b60450d8b06139af1
Shaun Minnihan <spminnihan@gmail.com>
2019-12-19 19:24:31 -0600

    user login complete

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,11 +1,29 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
 
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User newUser, String verify) {
+        if(!verify.equals(newUser.getPassword())) {
+            return "user/add";
+        }
+
+        model.addAttribute("user", newUser);
+        return "user/index";
+    }
 }
