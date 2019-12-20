bccefaae050f940ff6dadd54e543c092a3fe8baa
Shaun Minnihan <spminnihan@gmail.com>
2019-12-19 18:40:04 -0600

    added user model/controller and templates add and index

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,32 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private String userName;
+    private String emailAddress;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
