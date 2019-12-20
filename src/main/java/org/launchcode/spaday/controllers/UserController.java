391c3eef3e1e388abfc4ef35ea3f3e3f408fd9d5
Shaun Minnihan <spminnihan@gmail.com>
2019-12-19 20:11:09 -0600

    main studio complete

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,25 +3,27 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("error", false);
         return "user/add";
     }
 
     @PostMapping("add")
     public String processAddUserForm(Model model, @ModelAttribute User newUser, String verify) {
         if(!verify.equals(newUser.getPassword())) {
+            model.addAttribute("error", true);
+            model.addAttribute("userName", newUser.getUserName());
+            model.addAttribute("emailAddress", newUser.getEmailAddress());
             return "user/add";
         }
+
 
         model.addAttribute("user", newUser);
         return "user/index";
